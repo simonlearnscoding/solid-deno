01129c37ab3e0a9e1e32cc02a38d40a3e7df96a9
simon <simon.muscas@gmail.com>
2025-09-15 11:20:42 +0200

    make frontend accessible from server too

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,14 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [solid(), tailwindcss()],
-
   server: {
+    port: 5173,
+
+    host: "0.0.0.0",
     cors: false,
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: "http:0.0.0.0:8000",
         changeOrigin: true,
       },
     },
