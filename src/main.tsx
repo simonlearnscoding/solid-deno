25ec97c2c1080c45d42c3c7d8809d3267069f4ee
simonlearnscoding <simon.muscas@gmail.com>
2025-09-30 12:47:04 +0200

    tanstack router basic setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,20 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import App from "./App";
 import { createRoot } from "react-dom/client";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+
+// Import the generated route tree
+import { routeTree } from "./routeTree.gen";
 
 const queryClient = new QueryClient();
-
+const router = createRouter({ routeTree });
+// Register the router instance for type safety
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
 createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
-    <App />
+    <RouterProvider router={router} />
   </QueryClientProvider>,
 );
